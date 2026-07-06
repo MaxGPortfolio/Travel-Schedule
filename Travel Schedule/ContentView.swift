eecb6fce8144c8df491945fc17a1da3087c19d17
Max <gamanyuck.max@yandex.ru>
2026-07-06 19:48:45 +0700

    Created

---
+++ Travel Schedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel Schedule
+//
+//  Created by Максим on 24.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travel Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel Schedule
+//
+//  Created by Максим on 24.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
